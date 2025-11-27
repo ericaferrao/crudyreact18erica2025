28ba62e5d94426057c732c63ab5668126d606eea
Erica Stefny Ferrao <ferraoerica@gmail.com>
2025-11-26 21:36:41 -0800

    get post data and use promise to chain to then method and log and to catch error chain to catch  method

--- src/components/Post.js
+++ src/components/Post.js
@@ -3,6 +3,17 @@ import { getPosts } from "../services/postService";
 
 export default function Post() {
     const [posts, setPosts] = useState();
-    useEffect(() => { console.log("Load Post") }, [])// load post comestwice only decause strict mode is on
+    useEffect(() => {
+        getPosts()
+            .then((response) => {
+                console.log(response);
+            })
+            .catch((error) => {
+                console.log(error);// If you dont catch error 
+                // it will throw an unhandled promise rejection error
+                //and the screen gos black and red 
+            })
+
+    }, [])// load post comestwice only decause strict mode is on
     return (<></>)
 }

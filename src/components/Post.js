cd92e23d9a7e146dc636b4fda9eae5d85bfadd91
Erica Stefny Ferrao <ferraoerica@gmail.com>
2025-11-26 21:45:25 -0800

    Deisplay the fetched post on UI

--- src/components/Post.js
+++ src/components/Post.js
@@ -7,13 +7,23 @@ export default function Post() {
         getPosts()
             .then((response) => {
                 console.log(response);
+                setPosts(response.data)
             })
             .catch((error) => {
-                console.log(error);// If you dont catch error 
+                console.error(error);// If you dont catch error 
                 // it will throw an unhandled promise rejection error
                 //and the screen gos black and red 
             })
 
     }, [])// load post comestwice only decause strict mode is on
-    return (<></>)
+    return (<>
+        <h1>Posts</h1>
+        {!posts && <h2>Loading...</h2>}
+        {posts && posts.map((post) => (
+            <div key={post.id} style={{ border: '1px solid gray', marginBottom: '10px', padding: '10px' }}>
+                <h3>{post.title}</h3>
+                <p>{post.body}</p>
+            </div>
+        ))}
+    </>)
 }
--- src/services/postService.js
+++ src/services/postService.js
@@ -4,6 +4,6 @@ const api = axios.create({
     baseURL: 'https://jsonplaceholder.typicode.com',
 });
 
-const getPosts = () => api.get('/posts/1');
+const getPosts = () => api.get('/posts');
 
 export { getPosts }
